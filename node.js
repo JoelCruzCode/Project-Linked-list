ccd433b06a7395c84b30051bc7aff64dfe031cae
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-11-13 20:25:15 -0800

    create Node factory

---
+++ node.js
@@ -0,0 +1,25 @@
+function Node() {
+  let value = null;
+  let next = null;
+
+  return { value, next };
+  //   function setValue(v) {
+  //     value = v;
+  //   }
+
+  //   function getValue() {
+  //     return value;
+  //   }
+
+  //   function setNext(n) {
+  //     next = n;
+  //   }
+
+  //   function getNext() {
+  //     return next;
+  //   }
+
+  //   return { setValue, getValue, setNext, getNext };
+}
+
+export default Node;
